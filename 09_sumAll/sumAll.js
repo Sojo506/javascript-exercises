cfff5b40611ac9b64873569cff6177f30e68ac34
Sojo506 <fsojodev@gmail.com>
2025-09-11 14:27:36 -0600

    complete 09_sumAll challenge

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0) return 'ERROR';
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    if (typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
 
+    let max = Math.max(a, b);
+    let min = Math.min(a, b);
+    let sum = 0;
+
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
