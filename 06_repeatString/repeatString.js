60871512fddcdc6db0df891e02edb150992a8cd4
Sojo506 <fsojodev@gmail.com>
2025-09-11 10:31:50 -0600

    complete 06_repeatString challenge

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let aux = "";
+    if(num < 0) return "ERROR";
+    
+    for(let i = 0; i <num; i++){
+        aux += string;
+    }
 
+    return aux;
 };
 
 // Do not edit below this line
