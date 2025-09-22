2c58673e7c0ada41ba0e182dac333b76df4be902
Sojo506 <fsojodev@gmail.com>
2025-09-22 17:12:27 -0600

    complete 13_palindromes challenge

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/\p{P}+/gu, '').trim().split(' ').join('').toLowerCase();
+    aux = string.split('').reverse().join('');
 
+    return string == aux ? true : false;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
