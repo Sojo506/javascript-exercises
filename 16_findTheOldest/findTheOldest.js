2b32c56a292e6a5788e6fdf9bd63c619c79e029e
Sojo506 <fsojodev@gmail.com>
2025-09-23 16:09:11 -0600

    complete 16_findTheOldest challenge

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arrayOfPeople) {
+    return arrayOfPeople
+        .map(person => {
+            const death = person.hasOwnProperty('yearOfDeath') ? person.yearOfDeath : new Date().getFullYear();
+            return [person, death - person.yearOfBirth];
+        })
+        .reduce((prev, current) => {
+            return prev[1] < current[1] ? current : prev;
+        })[0];
 };
 
 // Do not edit below this line
