6c547b357bf22bf49bc4971822046a89fa1427a7
Sojo506 <fsojodev@gmail.com>
2025-09-23 15:24:35 -0600

    complete 14_fibonacci challenge

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0) return "OOPS"
+    
+    let current = 1;
+    let prev = 1;
+    let aux = 0;
 
+    for (let index = 0; index < n; index++) {
+        console.log(current)
+        console.log(prev)
+        aux = prev + current;
+        prev = current;
+        current = aux;
+    }
+
+    return current - prev;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
